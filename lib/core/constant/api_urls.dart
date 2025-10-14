ab6f6d9646afa2b037e03b29d0ce05f788046a3c
aliftheprogramer <alif.5220411242@student.uty.ac.id>
2025-10-15 02:46:54 +0700

    feat: Update API URLs for product and category endpoints

--- lib/core/constant/api_urls.dart
+++ lib/core/constant/api_urls.dart
@@ -3,8 +3,16 @@ class ApiUrls {
   static const String baseUrl = "https://mas-pos.appmedia.id/api/v1";
   static const String login = "$baseUrl/login";
   static const String profile = "$baseUrl/profile";
-  static const String category = "$baseUrl/category"; //get post put(id) delete(id)
+
+
+
+  //product
   static const String product = "$baseUrl/product"; //get post delete(id)
-  static const String updateProduct = "$baseUrl/product/update";  //(post but edit)
+  static const String productById = "$baseUrl/product/update";  //(post but edit)
+
+
+//category
+  static const String category = "$baseUrl/category"; //get post delete(id)
+  static const String categoryById = "$baseUrl/category/update"; //(post but edit)
 
 }
