4272303583ef85435a608f6e57320e95d5554a3b
aliftheprogramer <alif.5220411242@student.uty.ac.id>
2025-10-27 02:46:39 +0700

    wfowfe

---
+++ lib/features/home/presentation/pages/get_all_products_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:gmedia_project/core/services/services_locator.dart';
+import 'package:logger/logger.dart';
+
+class GetAllProductsScreen extends StatelessWidget {
+  const GetAllProductsScreen({super.key});
+
+  @override     
+  Widget build(BuildContext context) {
+    final logger = sl<Logger>();
+    return const SafeArea(child: 
+    );
+  }
+}
