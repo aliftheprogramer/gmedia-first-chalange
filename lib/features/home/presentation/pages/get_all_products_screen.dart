99424c0a5d8de53179f10f57cb3117a160c4c0d6
aliftheprogramer <alif.5220411242@student.uty.ac.id>
2025-10-29 11:35:38 +0700

    feat: Add ProductsPlaceholder widget for displaying product list with loading and error states

--- lib/features/home/presentation/pages/get_all_products_screen.dart
+++ lib/features/home/presentation/pages/get_all_products_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gmedia_project/features/category/presentation/widget/list_category_widget.dart';
 import 'package:gmedia_project/features/home/presentation/widget/custom_search_bar.dart';
+import 'package:gmedia_project/features/home/presentation/widget/get_all_products/get_category_product.dart';
 import 'package:gmedia_project/features/home/presentation/widget/home/home_app_bar.dart';
 import 'package:gmedia_project/navigation/cubit/navigation_cubit.dart';
 import 'package:gmedia_project/widget/custom_bottom_navigator.dart';
@@ -38,7 +39,7 @@ class GetAllProductsScreen extends StatelessWidget {
                   
 
               Expanded(
-                child: _ProductsPlaceholder(), // Ganti dengan grid/list produk kamu
+                child: ProductsPlaceholder(), // Ganti dengan grid/list produk kamu
               ),
             ],
           ),
@@ -50,23 +51,3 @@ class GetAllProductsScreen extends StatelessWidget {
   }
 }
 
-// Placeholder konten biar compile; ganti dengan widget produk kamu.
-class _ProductsPlaceholder extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 12,
-      separatorBuilder: (_, __) => const SizedBox(height: 8),
-      itemBuilder: (_, i) => Container(
-        height: 80,
-        alignment: Alignment.centerLeft,
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.grey.shade200,
-        ),
-        child: Text('Produk #$i'),
-      ),
-    );
-  }
-}
---
+++ lib/features/home/presentation/widget/get_all_products/get_category_product.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gmedia_project/core/services/services_locator.dart';
+import 'package:gmedia_project/features/home/presentation/cubit/product_sell/product_sell_cubit.dart';
+import 'package:gmedia_project/features/home/presentation/cubit/product_sell/product_sell_state.dart';
+import 'package:gmedia_project/features/home/presentation/widget/home/product_sell_widget.dart';
+import 'package:gmedia_project/features/product/domain/entity/product_entity_response.dart';
+import 'package:gmedia_project/features/product/domain/usecase/get_list_product_usecase.dart';
+
+class ProductsPlaceholder extends StatelessWidget {
+  const ProductsPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_){
+        final cubit = ProductSellCubit(sl <GetListProductUsecase>());
+        cubit.fetchSoldProducts();
+        return cubit;
+      },
+      child: BlocBuilder<ProductSellCubit, ProductSellState>(
+        builder: (context, state) {
+          if (state is ProductSellLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+      
+          if (state is ProductSellEmpty) {
+            return const Center(child: Text('Tidak ada produk yang dijual.'));
+          }
+      
+          if (state is ProductSellError) {
+            return Center(child: Text('Error: ${state.message}'));
+          }
+      
+          if (state is ProductSellLoaded) {
+            final products = state.products.cast<ProductEntityResponse>();
+            return GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 12,
+                childAspectRatio: 0.9,
+              ),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final p = products[index];
+                return ProductSellItem(product: p);
+              },
+            );
+          }
+      
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
